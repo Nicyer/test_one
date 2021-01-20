47c46ba08b82d446fca0744320bd593c27daeed4
nick <nicyer@sina.com>
2021-01-20 22:19:18 +0800

    verilog add test_demo

--- readme.v
+++ readme.v
@@ -3,6 +3,11 @@ module test(
 	input		sys_rst,
 	output		oled
 );
-
+reg	[7:0]	cnt;
+always@(posedge sysclk or negeddge ) begin 
+	if(!sys_clk) cnt <= 8'd0;
+	if(cnt < 8'hff) cnt <= cnt + 1'd1;
+	else cnt <= 8'd0;
+end 
 endmodule 
 
